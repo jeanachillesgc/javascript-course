5e14a99167ef1647c1f969d8f07c935e5b240e94
Jean Achille <jean.achille@hotmail.com>
2021-07-20 11:33:53 -0300

    Added the method calc() and pushOperation(), they'll be responsible for evaluating the expression inside the array _operation

--- Calculadora/scripts/controller/CalcController.js
+++ Calculadora/scripts/controller/CalcController.js
@@ -2,6 +2,7 @@ class CalcController{
 
     constructor(){
 
+        this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -27,17 +28,25 @@ class CalcController{
 
     }
 
+    /**
+     * Here we define one or more events(click // click drag), split it using
+     * the " " and add it into an array "events".
+     * ForEach event inside events we add a listener inside the element that
+     * we set in the method's parameter.
+     */
     addEventListenerAll(element, events, fn){
 
         events.split(" ").forEach(event => {
             element.addEventListener(event, fn, false);
         });
 
-    }
+    }    
 
     /**
      * Inside the #buttons and #parts ids we have the necessary to select all of 
-     * the calculator's buttons
+     * the calculator's buttons. In this method we select all of the g tags children
+     * of the #buttons and #parts, and add click and drag events on them using the
+     * method addEventListenerAll
     **/
     initButtonsEvents(){
 
@@ -46,7 +55,11 @@ class CalcController{
         buttons.forEach((btn, index)=>{
 
             this.addEventListenerAll(btn, "click drag", e=>{
-                console.log(btn.className.baseVal.replace("btn-", ""));
+
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
+
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
@@ -54,6 +67,138 @@ class CalcController{
             });
 
         });
+
+    }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+
+    }
+
+    getLastOperation(){
+        return this._operation[this._operation.length-1];
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length-1] = value;
+    }
+
+    isOperator(value){
+        return (["+", "-", "*", "%", "/"].indexOf(value) > -1);
+    }
+
+    pushOperation(value){
+
+        this._operation.push(value);
+
+        if(this._operation.length > 3){            
+            this.calc();
+        }
+    }
+    
+    calc(){
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(""));
+        this._operation = [result, last];
+    }
+
+    setLastNumberToDisplay(){
+        
+    }
+
+    addOperation(value){
+
+        if(isNaN(this.getLastOperation())){
+            //String
+            if(this.isOperator(value)){
+                this.setLastOperation(value);
+            }else if(isNaN(value)){
+                
+            }else{
+                this.pushOperation(value);
+            }
+
+        }else{
+
+            //Number
+            if(this.isOperator(value)){
+                this.pushOperation(value);
+            }else{
+                
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+                this.setLastNumberToDisplay();
+
+            }
+            
+        }
+
+
+        console.log(this._operation);
+
+    }
+
+    execBtn(value){
+
+        switch(value){
+
+            case "ac":
+                this.clearAll();
+                break;
+            case "ce":
+                this.clearEntry();
+                break;   
+            case "soma":
+                this.addOperation("+");
+                break;
+            case "subtracao":
+                this.addOperation("-");
+                break;
+            case "divisao":
+                this.addOperation("/");
+                break;
+            case "multiplicacao":
+                this.addOperation("*");
+                break;
+            case "porcento":
+                this.addOperation("%");
+                break;
+            case "igual":
+                
+                break;
+            case "ponto":
+                this.addOperation(".");
+                break;            
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+            case "0":
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+
+        }
 
     }
 
